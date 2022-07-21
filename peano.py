8cb311cb4f0a38a4db6cb314153483e81a2a4682
david reinfurt <reinfurt@o-r-g.com>
2022-07-21 10:22:51 -0400

    implement calculating X based on Peano notation * in process *

--- peano.py
+++ peano.py
@@ -5,25 +5,76 @@ import random
 import math
 
 class Point:
-    def __init__(self, n, T):
-        self.n = n
+    def __init__(self, T):
         self.T = T
         self.X = self.calc_X() 
         self.Y = self.calc_Y(T)
     
     def calc_X(self):
-        # requires implememtation
-        # t = k(0, 1)
-        digits = list(self.T)
-        for digit in digits:
-            print(digit, end='') 
+        # calc digits of X (left to right) from T
+        # following Peano's notation
+
+        # T = 0 . a[1]a[2]a[3]...
+        # X = 0 . b[1]b[2]b[3]...
+        # Y = 0 . c[1]c[2]c[3]...
+
+        # b[n] = k ^ (a[2] + a[4] ... a[2n-2]) a[2n-1]
+        
+        # with the exception that for arrays 0 is first member
+        # so here all indices in Peano are shifted -1
+        # but since range does not include the top value
+        # when iterating, also 2n-2 --> 2n-1 and 2n-1 ---> 2n
+
+        # T = 0 . a[0]a[1]a[2]...
+        # X = 0 . b[0]b[1]b[2]...
+        # Y = 0 . c[0]c[1]c[2]...
+
+        # b[n] = k ^ (a[1] + a[3] ... a[2*n-1]) a[2*n]
+    
+        X = ''                                          # string
+        a = list(self.T)                                # digits of T
+        b = [0 for i in range(int(len(self.T) / 2))]    # digits of X
+
+        # iterate through b (digits of X)
+
+        print('T :', self.T)
+
+        for n in range(len(b)):
+            e = 0               # k exponent sum or how many times to repeat k()
+            i = 0               # counter
+    
+            print('  n :', n)
+
+            # sum exponents
+            # or could have var for 2n-1 and check if greater than 0
+            # although actually ranges in python always are positive, see
+            # https://www.google.com/search?client=safari&rls=en&q=python+negative+value+for+range&ie=UTF-8&oe=UTF-8
+            # so perhaps leave without the catch condition
+            # maybe better name for k_sum_exp
+            # here 2 * n not 2 * n - 1 because range doesnt ever get to top value
+            # so putting 2 * n has the result of 2 * n - 1
+
+            for i in range(2 * n):
+                if i % 2 != 0:
+                    e += int(a[i])
+            
+            ###############
+            # ** k not quite working correctly, likely to do with repeats **
+            ###############
+
+            b[n] = k(int(a[2 * n]), e)
+
+            X += str(b[n])      # better to iterate thru b[] to populate X? or shortcut 
+                                # compress list to string like flatten
+
+            print('    a[', 2 * n, '] :', a[2 * n])
+            print('    e :', e)
+            print('    k(', a[n], ',', e, ') :', b[n])
+            
+        print('X :', X)
         print('')
-        # better to cycle through digits in a range
-        for i in range(self.n):
-            if i % 2 == 0:
-                print(i)
-                # print(digits[i])
-        print('')
+            
+        # return X
         return 1
 
     def calc_Y(self, t):
@@ -66,7 +117,6 @@ def main():
     os.system('clear')
     sleep(delay)
 
-    # number_of_points must be a power of 3
     while True:
         number_of_points = int(input('Number of points: '),10)
         if not is_power_of_three(number_of_points):
@@ -97,18 +147,20 @@ def main():
         T = base(n, 3)
         T = T.rjust(length, '0')      # pad zeros start
         T = T.ljust(length * 2, '0')  # pad zeros end
-        point = Point(n, T);
+        point = Point(T);
         points.append(point)
 
     # print('Calculating points ...')
 
+    # print results
+
     # could rjust pad this or use tabs
-    print('T -------> ( X , Y )')
-    print('')
-    for point in points:
+    # print('T -------> ( X , Y )')
+    # print('')
+    # for point in points:
         #  os.system('clear')
-        print(point.n, '    .', point.T, ' -------> (', point.X, ',', point.Y, ')')
-        sleep(delay)
-    print('')
+    #    print('.', point.T, ' -------> (', point.X, ',', point.Y, ')')
+    #    sleep(delay)
+    # print('')
 
 main()
